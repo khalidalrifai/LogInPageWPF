c2c9739267f7d97b2c3e995160f950bc39adfdc8
kalrifai <khalid.alrifai@alten.it>
2024-04-19 03:10:48 +1000

    Implement authentication handling

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace LogInPageWPF.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(LoginExecute, CanLoginExecute);
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+                (LoginCommand as RelayCommand)?.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+                (LoginCommand as RelayCommand)?.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        private bool CanLoginExecute(object parameter)
+        {
+            return !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
+        }
+
+        private void LoginExecute(object parameter)
+        {
+            if (Username == "admin" && Password == "admin")
+            {
+                MessageBox.Show("Login successful!", "Welcome", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Login failed. Check your username and password.", "Authentication Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
