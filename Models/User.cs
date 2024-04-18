596445954291a7639f0d2bf97a7a4f0d49870c17
kalrifai <khalid.alrifai@alten.it>
2024-04-19 03:28:42 +1000

    Implement User model with property notifications

---
+++ Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LogInPageWPF.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+        private string _email;
+
+        public User()
+        {
+        }
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
