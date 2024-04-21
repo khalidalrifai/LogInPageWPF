c33d44896cf17b1133778403fef8a5f9512bad74
kalrifai <khalid.alrifai@alten.it>
2024-04-22 09:05:18 +1000

    Fixed mismatching issues

--- Converters/InvertedBooleanToVisibilityConverter.cs
+++ Converters/InvertedBooleanToVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace LogInPageWPF.Views
+namespace LogInPageWPF.Converters
 {
     public class InvertedBooleanToVisibilityConverter : IValueConverter
     {
