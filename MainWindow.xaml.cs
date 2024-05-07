cc5fc8499ef472db5fe543acda438478e2f9164f
kalrifai <khalid.alrifai@alten.it>
2024-05-08 09:39:58 +1000

    Add Comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,20 +7,24 @@ namespace LogInPageWPF
 {
     public partial class MainWindow : Window
     {
+        // Main Constructor
         public MainWindow()
         {
-            InitializeComponent();
-            Loaded += MainWindow_Loaded;
+            InitializeComponent();          // To initializes component from XAML (Other files has thier similar initialization)
+            Loaded += MainWindow_Loaded;    // This is to attache an event handler for the Window Loaded event
         }
 
+        // Event handler for the Window's Loaded event
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            // Navigate to the LoginPage when the main window is loaded.
+            // This will automatically navigate to the LoginPage when the window is loaded
             MainFrame.Navigate(new LoginPage());
         }
 
+        // This is a method to navigate to different pages within the application
         public void NavigateTo(Page nextPage)
         {
+            // To use the Frame named 'MainFrame' to navigate to the specified page
             MainFrame.Navigate(nextPage);
         }
     }
